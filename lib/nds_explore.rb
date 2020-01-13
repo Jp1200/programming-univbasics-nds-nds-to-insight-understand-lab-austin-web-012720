4751eccad3750cb9ceff8cba33a25ad3bbf954c0
Jack Parker <jprkr96@gmail.com>
2020-01-13 17:45:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,7 +14,7 @@ def print_first_directors_movie_titles
     # pp nds[row_index]
     column_index = 0 
     while column_index < nds[row_index] do 
-      pp nds[row_index][column_index][0]
+      pp nds[row_index][column_index][:title]
       column_index += 1 
     end
     row_index += 1
